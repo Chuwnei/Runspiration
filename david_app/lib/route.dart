4963c18558b9c6190f7f5dfabac31ef6a6b8a316
Andrew Park <ajpark5@uci.edu>
2023-02-26 19:30:53 -0800

    Spinning Wheel Preparations
    There is currently a very strange bug regarding the addition of the spinning whell

--- david_app/lib/route.dart
+++ david_app/lib/route.dart
@@ -5,6 +5,7 @@ import 'package:david_app/userscreen.dart';
 import 'package:david_app/runscreen.dart';
 import 'package:david_app/profile.dart';
 import 'healthtest.dart';
+import 'spinningWheel.dart';
 
 var routeHolder = {
   '/': (context) => const Checker(),
@@ -14,4 +15,5 @@ var routeHolder = {
   '/runscreen': (context) => const RunScreen(),
   '/profilescreen': (context) => const ProfileScreen(),
   '/healthTest': (context) => const HealthTest(),
+  // '/spinningWheelScreen': (context) => SpinningWheelScreen(),
 };
